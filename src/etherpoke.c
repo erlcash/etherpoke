44d487dcacdf03acb9ac2c71df37ff0dd3a8c7e7
Earl Cash <erlcash@codeward.org>
2013-11-19 20:19:23 +0100

    Run arguments -cdhv added. Configuration file is required when running etherpoke, thus the constant ETHERPOKE_CONFIG is no longer required so it was removed.
    Help message is printed out when etherpoke is run with -h.
    Version information is printed out when etherpoke is run with -v.

--- src/etherpoke.c
+++ src/etherpoke.c
@@ -27,14 +27,14 @@
 #include <signal.h>
 #include <libconfig.h>
 #include <pthread.h>
+#include <unistd.h>
 
+#include "etherpoke.h"
 #include "queue.h"
 #include "config.h"
 
 #include "listener.h"
 #include "executioner.h"
-
-#define CONF_FILE "../conf/etherpoke.conf"
 
 conf_t *etherpoke_conf = NULL;
 
@@ -43,6 +43,26 @@ pthread_t *threads = NULL;
 queue_t packet_queue;
 pthread_mutex_t packet_queue_mut = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t packet_queue_cond = PTHREAD_COND_INITIALIZER;
+
+// Print help
+static
+void etherpoke_help (const char *p)
+{
+	fprintf (stdout, "%s v%s\n\nUsage:\n"
+					 "  %s [-dhv] -c <FILE>\n\n"
+					 "Options:\n"
+					 "  -c <FILE>\tconfiguration file\n"
+					 "  -d\t\trun as a daemon\n"
+					 "  -h\t\tshow this help text\n"
+					 "  -v\t\tshow version information\n"
+					 , p, ETHERPOKE_VER, p);
+}
+
+static
+void etherpoke_version (const char *p)
+{
+	fprintf (stdout, "%s v%s\n", p, ETHERPOKE_VER);
+}
 
 // Callback for signals leading to an end of the program
 static
@@ -66,12 +86,51 @@ main (int argc, char *argv[])
 	pthread_attr_t thread_attr;
 	listener_data_t *listener_data;
 	executioner_data_t executioner_data;
-	int i, th_rc;
+	char *config_file;
+	int i, c, th_rc;
 	
-	etherpoke_conf = conf_init (CONF_FILE);
+	config_file = NULL;
+	
+	while ( (c = getopt (argc, argv, "c:dhv")) != -1 ){
+		switch (c){
+			case 'c':
+				config_file = strdup (optarg);
+				
+				if ( config_file == NULL ){
+					fprintf (stderr, "%s: cannot allocate memory for configuration file\n", argv[0]);
+					abort ();
+				}
+				break;
+			
+			case 'd':
+				// run as a daemon
+				break;
+			
+			case 'h':
+				etherpoke_help (argv[0]);
+				exit (EXIT_SUCCESS);
+				break;
+			
+			case 'v':
+				etherpoke_version (argv[0]);
+				exit (EXIT_SUCCESS);
+				break;
+			
+			case '?':
+				break;
+		}
+	}
+	
+	if ( config_file == NULL ){
+		fprintf (stderr, "%s: configuration file not specified. Use '-h' to see help.\n", argv[0]);
+		exit (EXIT_FAILURE);
+	}
+	
+	etherpoke_conf = conf_init (config_file);
 	
 	if ( etherpoke_conf == NULL ){
-		fprintf (stderr, "%s: cannot load configuration file '%s'\n", argv[0], CONF_FILE);
+		fprintf (stderr, "%s: cannot load configuration file '%s'.\n", argv[0], config_file);
+		free (config_file);
 		exit (EXIT_FAILURE);
 	}
 	
@@ -129,7 +188,7 @@ main (int argc, char *argv[])
 	
 	// Wait for threads to finish (don't forget to wait for executioner, hence + 1)
 	for ( i = 0; i < etherpoke_conf->interfaces_count + 1; i++ ){
-		th_rc = pthread_join (threads[i], NULL); // maybe we could take a look at the status returned from thread?
+		th_rc = pthread_join (threads[i], NULL); // maybe we could (should) take a look at the status returned from thread?
 		
 		if ( th_rc != 0 ){
 			fprintf (stderr, "%s: cannot join with the threads\n", argv[0]);
@@ -139,7 +198,9 @@ main (int argc, char *argv[])
 	
 	free (threads);
 	free (listener_data);
+	queue_destroy (&packet_queue);
 	conf_destroy (etherpoke_conf);
+	free (config_file);
 	
 	pthread_exit ((void*) 0);
 }
---
+++ src/etherpoke.h
@@ -0,0 +1,6 @@
+#ifndef _ETHERPOKE_H
+#define _ETHERPOKE_H
+
+#define ETHERPOKE_VER "0.1"
+
+#endif
