a30118d143f9488c9683c60e248839671598d68d
antagon <antagon@codeward.org>
2015-11-23 04:14:45 +0100

    Hostformat parser.
    This is helper function to split HOSTNAME:PORT formatted string.

---
+++ src/hostformat.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <limits.h>
+#include <errno.h>
+
+int
+hostformat_parse (const char *str, char *hostname, int *port)
+{
+	char *semicolon_pos, *invchar_pos;
+	size_t cpy_cnt;
+
+	semicolon_pos = strchr (str, ':');
+
+	if ( semicolon_pos == NULL )
+		return -1;
+
+	cpy_cnt = semicolon_pos - str;
+
+	if ( cpy_cnt > HOST_NAME_MAX )
+		cpy_cnt = HOST_NAME_MAX - 1;
+
+	strncpy (hostname, str, cpy_cnt);
+	hostname[cpy_cnt] = '\0';
+
+	errno = 0;
+	invchar_pos = NULL;
+
+	*port = strtol ((semicolon_pos + 1), &invchar_pos, 10);
+
+	if ( *port == 0 || *invchar_pos != '\0' || errno == ERANGE )
+		return -1;
+
+	return 0;
+}
+
---
+++ src/hostformat.h
@@ -0,0 +1,7 @@
+#ifndef _HOSTFORMAT_H
+#define _HOSTFORMAT_H
+
+extern int hostformat_parse (const char *str, char *hostname, int *port);
+
+#endif
+
